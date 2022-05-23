887f45b26dded5b1103fd1d70bca2871777949ca
Allenchew <csoonzhi69@yahoo.com>
2022-05-23 21:35:21 +0800

    Added Project's base
    Added some source to use for background and added simple script for movement.

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement
+{
+    private float defaultSpeed = 1.0f;
+    public float MovementSpeed
+    {
+        get { return defaultSpeed; }
+        set { defaultSpeed = value; }
+    }
+
+    public void MoveRight(GameObject gameObject)
+    {
+        gameObject.transform.localPosition += gameObject.transform.right * defaultSpeed;
+    }
+    public void MoveLeft(GameObject gameObject)
+    {
+        gameObject.transform.localPosition -= gameObject.transform.right * defaultSpeed;
+    }
+    public void Jump(GameObject gameObject)
+    {
+
+    }
+}
+
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Movement characterMovement = new Movement();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            characterMovement.MoveRight(gameObject);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            characterMovement.MoveLeft(gameObject);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("pressed");
+        }else if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("pressing..");
+        }
+    }
+}
